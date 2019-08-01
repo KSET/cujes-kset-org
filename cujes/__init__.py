4a64e4fe635bf3abc73324542ac52ca7c4e8d3e2
LVH-27 <leonard.volaric-horvat@fer.hr>
2019-08-01 15:51:14 +0200

    Added database support.
    Created a SQLAlchemy instance in __init__.py, configured connecting to
    the database in config.py, and added the models in models.py
    
    Several DB-related environment variables were added in .flaskenv,
    including the password. This is to facilitate development, but in the future
    this should obviously be removed from .flaskenv.

--- cujes/__init__.py
+++ cujes/__init__.py
@@ -1,5 +1,12 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from config import Config
 
 cujes_app = Flask(__name__)
+cujes_app.config.from_object(Config)
+db = SQLAlchemy(cujes_app)
+migrate = Migrate(cujes_app, db)
 
-from cujes import routes
+from cujes import routes  # noqa
+# noqa makes flake8 ignore the PEP-8-non-compliant import
---
+++ cujes/config.py
@@ -0,0 +1,15 @@
+import os
+
+
+class Config:
+    SECRET_KEY = os.environ.get("SECRET_KEY") or 'mali-_i_-veliki-pimpeki'
+    # URI format: dialect+driver://username:password@host:port/database
+    DB_USER = os.environ.get("DB_USER") or "cujes"
+    DB_PASSWORD = os.environ.get("DB_PASSWORD")
+    DB_HOST = os.environ.get("DB_HOST")
+    DB_PORT = os.environ.get("DB_PORT") or "5432"
+    SQLALCHEMY_DATABASE_URI =\
+        "postgresql://{}:{}@{}:{}/{}".format(DB_USER,
+                                             DB_PASSWORD,
+                                             DB_HOST,
+                                             DB_PORT)
