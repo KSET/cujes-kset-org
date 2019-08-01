caf7f21a9a705e94cb6699933da67fa53295f153
LVH-27 <leonard.volaric-horvat@fer.hr>
2019-08-01 16:22:24 +0200

    Forgot to add DB_NAME to DB config

--- config.py
+++ config.py
@@ -8,8 +8,9 @@ class Config:
     DB_PASSWORD = os.environ.get("DB_PASSWORD")
     DB_HOST = os.environ.get("DB_HOST")
     DB_PORT = os.environ.get("DB_PORT") or "5432"
-    SQLALCHEMY_DATABASE_URI =\
-        "postgresql://{}:{}@{}:{}/{}".format(DB_USER,
-                                             DB_PASSWORD,
-                                             DB_HOST,
-                                             DB_PORT)
+    DB_NAME = os.environ.get("DB_NAME")
+    SQLALCHEMY_DATABASE_URI = "postgresql://{}:{}@{}:{}/{}".format(DB_USER,
+                                                                   DB_PASSWORD,
+                                                                   DB_HOST,
+                                                                   DB_PORT,
+                                                                   DB_NAME)
